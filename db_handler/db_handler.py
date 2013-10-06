a0858795b6e3197bdd27d4d28fb8763a2c796018
paulm <paulm@freegeek.org>
2013-10-05 18:14:33 -0700

    added get_dir_list

--- db_handler/db_handler.py
+++ db_handler/db_handler.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+import glob
 import os
 import pickledb
+
+class MyError(Exception):
+    pass
+
 def get_directory_size(start_path):
     '''get size of directory and contents'''
     total_size = 0
@@ -9,3 +14,13 @@ def get_directory_size(start_path):
             fp = os.path.join(dirpath, f)
             total_size += os.path.getsize(fp)
     return total_size
+
+def get_directory_list(dpath):
+    '''returns a list of directories in a directory, 
+    with fully qualified paths'''
+    if not os.path.isdir(dpath):
+        raise MyError('oops %s is not a directory'% dpath)
+    dirs = [os.path.join(dpath, filename) for filename in os.listdir(dpath)  if (os.path.isdir(os.path.join(dpath, filename)))]
+    return dirs
+
+
